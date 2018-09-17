62cdebe8040875b29b47a437c57d53f62e247dbf
hadidy100 <hadidy100@gmail.com>
2018-09-16 20:58:39 -0700

    Added api_v2.py to the project where I added on to David's work and consolidated the GET and the POST methods under one route, but more testing to come

--- init.sql
+++ init.sql
@@ -14,9 +14,9 @@ CREATE TABLE forums(
     name TEXT,
     creator INTEGER NOT NULL,
     timestamp TEXT,
-    FOREIGN KEY (creator) REFERNECES USERS(id)
+    FOREIGN KEY (creator) REFERENCES USERS(id)
 );
---threads table also references users and forums by their id so normalization reasons as well 
+--threads table also references users and forums by their id so normalization reasons as well
 CREATE TABLE threads(
     id INTEGER PRIMARY KEY,
     title TEXT,
@@ -39,8 +39,8 @@ INSERT INTO users(username,password) VALUES('jon','password');
 
 --isert 3 sample forums where each one is created by one of the sample users above
 INSERT INTO forums(name,creator) VALUES('redis',(select id from users where username = 'david'));
-INSERT INTO forums(name,creator) VALUES('mongodb',(select id form users where username ='jon'));
-INSERT INTO forums(name,creator) VALUES('Oracle DB',(select id form users where username ='Anas'));
+INSERT INTO forums(name,creator) VALUES('mongodb',(select id from users where username ='jon'));
+INSERT INTO forums(name,creator) VALUES('Oracle DB',(select id from users where username ='anas'));
 
 
 --insert 3 sample threads for each one of the sample forums that are created above
@@ -60,7 +60,7 @@ INSERT INTO threads
     'HELP',
     'Why do I get an error when I ping to redis-cli??, please help!!',
     (select id from users where username = 'david'),
-    (select forumId from forums where name = 'redis'),
+    (select id from forums where name = 'redis'),
     DATETIME('now','localtime')
   );
 --Thead #2
@@ -77,7 +77,7 @@ INSERT INTO threads
       'mongodb vs hadoop',
       'Can anyone please tell what is the difference between mangoDB and Hadoop? ',
       (select id from users where username = 'jon'),
-      (select forumId from forums where name = 'mongodb'),
+      (select id from forums where name = 'mongodb'),
       DATETIME('now','localtime')
     );
 --Thead #3
@@ -95,6 +95,23 @@ INSERT INTO threads
         'Why would any enterprise chooses to use Oracle instead of mySql is byond me,
         I mean it is a lot more expesive, and I do not see any performance or capability issues, thoughts? Anyone? ',
         (select id from users where username = 'anas'),
-        (select forumId from forums where name = 'Oracle DB'),
+        (select id from forums where name = 'Oracle DB'),
         DATETIME('now','localtime')
       );
+      --Thead #4
+          INSERT INTO threads
+          (
+            title,
+            comment,
+            creator,
+            forumid,
+            timestamp
+          )
+            VALUES
+            (
+              'SQL Lite',
+              'How handy is this SQL lite thing, I must admit, I have never used it before, but I love it!!! ',
+              (select id from users where username = 'anas'),
+              (select id from forums where name = 'Oracle DB'),
+              DATETIME('now','localtime')
+            );
