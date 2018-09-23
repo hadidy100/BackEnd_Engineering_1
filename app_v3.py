817df4c8b36c2e5887e523c1150107818dc6535b
hadidy100 <hadidy100@gmail.com>
2018-09-23 03:22:36 -0700

    added app_v3 to the code, and improved the database schemas

---
+++ app_v3.py
@@ -0,0 +1,106 @@
+import flask, click, sqlite3, sys ,datetime
+from flask import g, request, jsonify, json, render_template
+from flask_basicauth import BasicAuth
+
+app = flask.Flask(__name__)
+app.debug = True
+#create all of my classes here*******************************************************
+#authentication class
+#subclass of BasicAuth, with check_credentials function overwritten
+class Auth(BasicAuth):
+    def check_credentials(self,username,password):
+        query = "SELECT username,password,id FROM users WHERE"
+        to_filter = []
+        if username:
+            query += ' username=? AND'
+            to_filter.append(username)
+        if password:
+            query += ' password=? ;'
+            to_filter.append(password)
+        conn = sqlite3.connect('forums.db')
+        cur = conn.cursor()
+        results = cur.execute(query, to_filter).fetchall()
+        return str(results[0])
+
+def newForum(name,creator):
+    conn = sqlite3.connect('forums.db')
+    cur = conn.cursor()
+    now = datetime.datetime.now()
+    currentDate =  now.strftime("%B %d, %Y")
+    print(currentDate)
+    cur.execute('INSERT INTO forums (name, creator,timestamp) VALUES (?, ?, ?)', [name, creator, currentDate])
+    conn.commit()
+    msg = "Record successfully added"
+    #conn.close()
+    return msg
+
+#end of my classes******************************************************************
+            #The following are variables that contain the queries that we will use within the upcomming methods
+            #we grouped them here so that we can refer to them by name only and simplifiy the code, beside grouping them here
+            # will simplify the process if we need to mofidy the query, view it, and refactoringself.
+#***********************************************************************************
+getUserid = 'select u.id username from users u where u.username = ?'
+getAllForums = 'SELECT f.id id, f.name name, u.username username FROM forums f, users u where u.id = f.creator;'
+getAspecificFourm = 'SELECT u.username username, f.name name, f.timestamp timestamp FROM forums f, users u where u.id = f.creator and f.id = ? order by f.timestamp;'
+getAllThreadsWithinAforum = 'SELECT t.id id,t.title title ,u.username username,f.name,t.timestamp timestamp FROM forums f,users u,threads t where u.id = t.creator  and t.forumid = f.id and f.id = ? order by t.timestamp;'
+#***********************************************************************************
+#instantiate Auth class
+basic_auth = Auth(app)
+#***********************************************************************************
+# Problems one and two, for route /forums and handling both GET and POST
+@app.route('/forums',methods=['GET','POST'])
+def showOrForums():
+        conn = sqlite3.connect('forums.db')
+        cur = conn.cursor()
+        #use the query string that we created in the begining
+        if request.method == 'GET':
+            results = cur.execute(getAllForums).fetchall()
+            #convert the result into JSON
+            return jsonify(results)
+        if request.method == 'POST':
+                    #get username and password from Authorization header
+            user = request.authorization.username
+            print(user)
+            userId = cur.execute([getUserid], (user)).fetchall()
+            password = request.authorization.password
+                    #authenticate user
+            auth = Auth()
+            validUser = len(auth.check_credentials(user,password))
+            if  validUser <= 2:
+                return 'invalid login'
+            elif validUser > 2:
+                data = request.get_json()
+                forumName = data['name']
+                newForum([forumName], (userId))
+                return jsonify (data)
+#***********************************************************************************
+@app.route('/forums/<int:forum_Id>',methods=['GET','POST'])
+def showThreadsWithinForum (forum_Id):
+        conn = sqlite3.connect('forums.db')
+        cur = conn.cursor()
+        #use the query string that we created in the begining
+        if request.method == 'GET':
+            intForumId = int(forum_Id)
+            print (forum_Id)
+            print (intForumId)
+            results = cur.execute([getAllThreadsWithinAforum],(intForumId)).fetchall()
+            #convert the result into JSON
+            return jsonify(results)
+        if request.method == 'POST':
+                    #get username and password from Authorization header
+            user = request.authorization.username
+            print(user)
+            #userId = cur.execute(getUserid, user).fetchall()
+            password = request.authorization.password
+                    #authenticate user
+            auth = Auth()
+            validUser = len(auth.check_credentials(user,password))
+            if  validUser <= 2:
+                return 'invalid login'
+            elif validUser > 2:
+                data = request.get_json()
+                forumName = data['name']
+                newThread(forumName, 1)
+                return jsonify (data)
+if __name__ == '__main__':
+    app.run()
--- init.sql
+++ init.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS threadPosts;
+DROP TABLE IF EXISTS threads;
 DROP TABLE IF EXISTS forums;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS threads;
+
+
 -- users table which will be referenced by forums and threads tables
 CREATE TABLE users(
     id INTEGER PRIMARY KEY,
@@ -20,17 +23,22 @@ CREATE TABLE forums(
 CREATE TABLE threads(
     id INTEGER PRIMARY KEY,
     title TEXT,
-    comment TEXT,
     creator INTEGER,
     forumId INTEGER,
     timestamp TEXT,
     foreign key (creator) REFERENCES users (id),
     foreign key (forumId) REFERENCES forums (id)
 );
-
-CREATE UNIQUE INDEX idx_name ON forums (name);
-CREATE UNIQUE INDEX idx_user ON users (username);
-CREATE UNIQUE INDEX idx_thread ON threads (title);
+--threadComments referrenced by thread Id , but each row has its own Id to enable multiple posts for the same thread and the same createForum
+CREATE TABLE threadPosts(
+    id integer PRIMARY KEY,
+    threadId INTEGER,
+    creator INTEGER,
+    comment TEXT,
+    timestamp TEXT,
+    foreign key (threadId) REFERENCES threads (id),
+    foreign key (creator) REFERENCES users(id)
+);
 
 -- isnert 3 sample users
 INSERT INTO users(username,password) VALUES('david','password');
@@ -38,19 +46,18 @@ INSERT INTO users(username,password) VALUES('anas','password');
 INSERT INTO users(username,password) VALUES('jon','password');
 commit;
 --isert 3 sample forums where each one is created by one of the sample users above
-INSERT INTO forums(name,creator) VALUES('redis',(select id from users where username = 'david'),DATETIME('now','localtime'));
-INSERT INTO forums(name,creator) VALUES('mongodb',(select id from users where username ='jon'),DATETIME('now','localtime'));
-INSERT INTO forums(name,creator) VALUES('Oracle DB',(select id from users where username ='anas'),DATETIME('now','localtime'));
+INSERT INTO forums(name,creator,timestamp) VALUES('redis',(select id from users where username = 'david'),DATETIME('now','localtime'));
+INSERT INTO forums(name,creator,timestamp) VALUES('mongodb',(select id from users where username ='jon'),DATETIME('now','localtime'));
+INSERT INTO forums(name,creator,timestamp) VALUES('Oracle DB',(select id from users where username ='anas'),DATETIME('now','localtime'));
 
 
---insert 3 sample threads for each one of the sample forums that are created above
+--insert 7 sample threads for each one of the sample forums that are created above
 --Each thread has the forum id so that we can see what forum does this thread belog to,
--- in addition to having a creator id so that we know who created that thread
+-- in addition to having a creator id so that we know who created that thread but the actual text inside the thread is pushed into another table called threadPosts
 
 INSERT INTO threads
 (
   title,
-  comment,
   creator,
   forumid,
   timestamp
@@ -58,61 +65,285 @@ INSERT INTO threads
   VALUES
   (
     'HELP',
-    'Why do I get an error when I ping to redis-cli??, please help!!',
     (select id from users where username = 'david'),
     (select id from forums where name = 'redis'),
     DATETIME('now','localtime')
   );
---Thead #2
-  INSERT INTO threads
+  --******* Thread posts within a THREAD
+  INSERT INTO threadPosts
   (
-    title,
-    comment,
+    threadId,
     creator,
-    forumid,
+    comment,
     timestamp
   )
     VALUES
     (
-      'mongodb vs hadoop',
-      'Can anyone please tell what is the difference between mangoDB and Hadoop? ',
-      (select id from users where username = 'jon'),
-      (select id from forums where name = 'mongodb'),
+      1,
+      1,
+      'I can not start Redis, I ping to the IP address, but I get no response',
       DATETIME('now','localtime')
     );
---Thead #3
-    INSERT INTO threads
+    INSERT INTO threadPosts
     (
-      title,
-      comment,
+      threadId,
       creator,
-      forumid,
+      comment,
       timestamp
     )
-      VALUES
-      (
-        'Price',
-        'Why would any enterprise chooses to use Oracle instead of mySql is byond me,
-        I mean it is a lot more expesive, and I do not see any performance or capability issues, thoughts? Anyone? ',
-        (select id from users where username = 'anas'),
-        (select id from forums where name = 'Oracle DB'),
-        DATETIME('now','localtime')
-      );
-      --Thead #4
-          INSERT INTO threads
+    VALUES
+    (
+      1,
+      3,
+      'May be you are entering the wrong ip address or the wrong host name, can you share with us what you typed?',
+      DATETIME('now','localtime')
+    );
+          -- thread post for Redis
+        INSERT INTO threadPosts
+        (
+        threadId,
+        creator,
+        comment,
+        timestamp
+        )
+        VALUES
+        (
+           1,
+           2,
+          'Me too, I can not start Redis, I tried all the instruction that were in the documentation but I get server error code 500, please help!!',
+          DATETIME('now','localtime')
+        );
+--Thead #2
+INSERT INTO threads
+(
+  title,
+  creator,
+  forumid,
+  timestamp
+)
+  VALUES
+  (
+    'mongodb vs hadoop',
+    3,
+    (select id from forums where name = 'mongodb'),
+    DATETIME('now','localtime')
+  );
+        --Thread posts within a THREAD
+        INSERT INTO threadPosts
+                  (
+                    threadId,
+                    creator,
+                    comment,
+                    timestamp
+                  )
+                    VALUES
+                    (
+                      2,
+                      3,
+                      'Can anyone please tell what is the difference between mangoDB and Hadoop? ',
+                      DATETIME('now','localtime')
+                      );
+          INSERT INTO threadPosts
+                    (
+                      threadId,
+                      creator,
+                      comment,
+                      timestamp
+                    )
+                      VALUES
+                      (
+                        2,
+                        2,
+                        'I have the same question, you took the words out of my mouth! ',
+                        DATETIME('now','localtime')
+                        );
+--Thead #3
+INSERT INTO threads
+(
+  title,
+  creator,
+  forumid,
+  timestamp
+)
+VALUES
+(
+  'Price',
+  2,
+  (select id from forums where name = 'Oracle DB'),
+  DATETIME('now','localtime')
+);
+        INSERT INTO threadPosts
+        (
+          threadId,
+          creator,
+          comment,
+          timestamp
+        )
+        VALUES
+        (
+          3,
+          2,
+          'Why would any enterprise chooses to use Oracle instead of mySql is byond me, I mean it is a lot more expesive, and I do not see any performance or capability issues, thoughts? Anyone? ',
+          DATETIME('now','localtime')
+        );
+        --Thead #4
+INSERT INTO threads
+(
+  title,
+  creator,
+  forumid,
+  timestamp
+)
+VALUES
+(
+  'SQL Lite',
+  2,
+  (select id from forums where name = 'Oracle DB'),
+  DATETIME('now','localtime')
+);
+        INSERT INTO threadPosts
+        (
+          threadId,
+          creator,
+          comment,
+          timestamp
+        )
+        VALUES
+        (
+          4,
+          2,
+          'How handy is this SQL lite thing, I must admit, I have never used it before, but I love it!!! ',
+          DATETIME('now','localtime')
+        );
+--thead 5
+INSERT INTO threads
+(
+  title,
+  creator,
+  forumid,
+  timestamp
+)
+VALUES
+(
+  'Does anyone know how to start Redis?',
+  (select id from users where username = 'david'),
+  (select id from forums where name = 'redis'),
+  DATETIME('now','localtime')
+);
+        INSERT INTO threadPosts
+        (
+          threadId,
+          creator,
+          comment,
+          timestamp
+        )
+        VALUES
+        (
+           5,
+           2,
+          'I Think its redis-ctl start, but I am not sure',
+          DATETIME('now','localtime')
+        );
+
+INSERT INTO threads
+(
+  title,
+  creator,
+  forumid,
+  timestamp
+)
+VALUES
+(
+  'Has anyone heard of Edis?',
+   3,
+  (select id from forums where name = 'redis'),
+  DATETIME('now','localtime')
+);
+
+        INSERT INTO threadPosts
+        (
+          threadId,
+          creator,
+          comment,
+          timestamp
+        )
+        VALUES
+        (
+           6,
+           3,
+          'I keep hearing about this Edis, what exactly is it? And how different is it from Redis?',
+          DATETIME('now','localtime')
+        );
+        -- thread # 7
+          INSERT INTO threadPosts
           (
-            title,
-            comment,
+            threadId,
             creator,
-            forumid,
+            comment,
+            timestamp
+          )
+          VALUES
+          (
+             6,
+             1,
+            'I can not tell you why so many confusing names, Redis, Pedis, Ennis, Whats going on????',
+            DATETIME('now','localtime')
+          );
+          INSERT INTO threadPosts
+          (
+            threadId,
+            creator,
+            comment,
             timestamp
           )
             VALUES
             (
-              'SQL Lite',
-              'How handy is this SQL lite thing, I must admit, I have never used it before, but I love it!!! ',
-              (select id from users where username = 'anas'),
-              (select id from forums where name = 'Oracle DB'),
+               6,
+               2,
+              'According to this website, https://dbdb.io, Edis an an Erlang re-implementation of Redis',
               DATETIME('now','localtime')
             );
-            commit;
+commit;
+/*
+ select
+	 f.name,
+	 u.username,
+	 tp.comment,
+	 t.title,
+	 tp.timestamp
+from
+	 forums f,
+	 users u,
+	 threads t,
+	 threadPosts tp
+where
+	f.id = t.forumId and
+	u.id = t.creator and
+	t.id = tp.threadId
+order by
+	tp.timestamp;
+
+
+
+  select
+	 f.name forumName,
+	 u.username froumCreator,
+	 tp.comment,
+	 t.title,
+	 tp.timestamp,
+	 f.id forumId,
+	 (select uu.username from users uu, threadPosts ttp where ttp.creator = uu.id and ttp.id = tp.id) PostCreator,
+	 (select uuu.username from users uuu, threads ttt  where ttt.creator = uuu.id and ttt.id = t.id) threadCreator
+from
+	 forums f,
+	 users u,
+	 threads t,
+	 threadPosts tp
+where
+	f.id = t.forumId and
+	u.id = t.creator and
+	t.id = tp.threadId and
+	f.creator = u.id
+order by
+	tp.timestamp,  f.name;
+*/
